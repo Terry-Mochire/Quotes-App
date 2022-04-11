9e6e1ec2cfe2e8b10dd07b121cd69a2206f33c78
Terry <terrybmochire@gmail.com>
2022-04-11 04:10:58 +0300

    Auto-generated commit [ci skip]

--- src/app/quote.ts
+++
@@ -1,8 +0,0 @@
-export class Quote {
-       showDescription: Boolean;
-        creationTime: any;
-    constructor(public quote: string, public quoteAuthor: string, public userName: string, public upvote:number, public downvote:number) {
-        this.showDescription =false;
-        this.creationTime = new Date().getTime();
-    }
-}
